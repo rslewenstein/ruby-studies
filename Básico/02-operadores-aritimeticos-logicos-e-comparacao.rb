5e619a05dafe3c1c0e50298eb4533f76aff1911a
rslewenstein <rafasoaresjf@gmail.com>
2025-10-04 14:42:14 -0300

    operadores

---
+++ Básico/02-operadores-aritimeticos-logicos-e-comparacao.rb
@@ -0,0 +1,17 @@
+# Aritméticos
+puts 10 + 5
+puts 10 / 3
+puts 10.0 / 3
+puts 2 ** 4 
+# No C#, não existe **, só Math.Pow.
+
+# Comparação
+puts 7 == 7    # true
+puts 7 != 5    # true
+puts 5 <=> 7   # -1
+# O <=> é chamado de operador espaçonave (spaceship operator) → muito usado em ordenação.
+
+# Lógicos
+x = 10
+puts x > 5 && x < 20   # true
+puts !(x == 10)        # false
