d1f1d55f4cb37fedf501f6ba7558fd315659de80
rslewenstein <rafasoaresjf@gmail.com>
2025-10-06 18:44:50 -0300

    adicionando a view

---
+++ 06.Pratica-e-projetos/002.mvc-puro-projeto-cadastro/view/produtos_view.rb
@@ -0,0 +1,20 @@
+class ProdutosView
+    def mostrar_lista(produtos)
+        puts "=== Lista de Produtos ==="
+        produtos.each do |p|
+            puts "Nome: #{p.nome} | Preço: R$ #{'%.2f' % p.preco}"
+        end
+        puts "========================================="
+    end 
+    
+    def solicitar_dados
+        print "Digite o nome do produto: "
+        nome = gets.chomp
+        print "Digite o preço: 0"
+        preco = gets.chomp.to_f
+        { nome: nome, preco: preco }
+    end 
+    
+    def mensagem_sucesso
+        puts "\u2705 Produto cadastrado com sucesso!"
+end    
